86843c41a66ca2c90d6c1f45c6ec7293c03eb6f8
danglh1910elmk <danglh1910elmk@gmail.com>
2025-08-17 14:23:48 +0700

    Day 23: prevent XSS

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const todoForm = document.querySelector(".todo-form");
 const todoInput = document.getElementById("todo-input");
 const taskList = document.getElementById("task-list");
 
-// console.log(todoForm, todoInput, taskList);
-
 const tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
 
 renderTaskList();
@@ -19,7 +17,7 @@ todoForm.onsubmit = (e) => {
 
     // add new task to task array, also prevent adding the same task name and empty task
     const hasSameTask = tasks.find((task) => {
-        return newTask.name === task.name;
+        return newTask.name.toLowerCase() === task.name.toLowerCase();
     });
     if (!newTask.name) {
         alert("Do not add empty task!");
@@ -43,6 +41,8 @@ todoForm.onsubmit = (e) => {
 
     // save data to localStorage
     localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    todoInput.focus();
 };
 
 function renderTaskList() {
@@ -51,17 +51,49 @@ function renderTaskList() {
         return;
     }
 
-    const html = tasks
-        .map((task) => {
-            return `<li class="task-item">
-                        <span class="task-title">${task.name}</span>
-                        <div class="task-action">
-                            <button class="task-btn edit">Edit</button>
-                            <button class="task-btn done">Mark as done</button>
-                            <button class="task-btn delete">Delete</button>
-                        </div>
-                    </li>`;
-        })
-        .join("");
-    taskList.innerHTML = html;
+    // const html = tasks
+    //     .map((task) => {
+    //         return `<li class="task-item">
+    //                     <span class="task-title">${task.name}</span>
+    //                     <div class="task-action">
+    //                         <button class="task-btn edit">Edit</button>
+    //                         <button class="task-btn done">Mark as done</button>
+    //                         <button class="task-btn delete">Delete</button>
+    //                     </div>
+    //                 </li>`;
+    //     })
+    //     .join("");
+    // taskList.innerHTML = html;
+
+    //
+    taskList.innerHTML = "";
+
+    tasks.forEach((task) => {
+        const li = document.createElement("li");
+        li.className = "task-item";
+        taskList.appendChild(li);
+
+        const span = document.createElement("span");
+        span.className = "task-title";
+        span.textContent = task.name; // prevent XSS
+
+        const div = document.createElement("div");
+        div.className = "task-action";
+
+        li.append(span, div);
+
+        const btn1 = document.createElement("button");
+        btn1.className = "task-btn edit";
+        btn1.innerText = "Edit";
+
+        const btn2 = document.createElement("button");
+        btn2.className = "task-btn done";
+        btn2.innerText = "Mark as done";
+
+        const btn3 = document.createElement("button");
+        btn3.className = "task-btn delete";
+        btn3.innerText = "Delete";
+
+        div.append(btn1, btn2, btn3);
+    });
 }
